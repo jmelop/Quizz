c32194bf424b826c771b82d8c6a8f3e89c8e07a7
Juan Melo Pérez <juanmlstm@gmail.com>
2022-05-04 10:31:14 +0100

    New dropdown button in navbar. Code refactoring

--- src/app/components/user/navbar/navbar.component.ts
+++ src/app/components/user/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,28 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public saveCategory(): void {
+    Swal.fire({
+      title: "New category!",
+      text: "Category:",
+      input: 'text',
+      showCancelButton: true
+    }).then((result) => {
+      if (result.value) { }
+    });
+  }
+
+  public saveLanguage(): void {
+    Swal.fire({
+      title: "New language!",
+      text: "Language:",
+      input: 'text',
+      showCancelButton: true
+    }).then((result) => {
+      if (result.value) { }
+    });
   }
 
 }
